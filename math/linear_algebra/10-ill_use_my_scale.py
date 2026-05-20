d03acdcd39756c6cf11f57271ec34d448f1d3572
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-20 14:56:17 +0000

    using numpy library to calcualte shape of matrix, without indexing, looping etc

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""using numpy build-in function to calculate shape of matrix."""
+
+
+def np_shape(matrix):
+    """this is documentation for shape calculation of any matrix."""
+    return matrix.shape
