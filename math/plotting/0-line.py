c733dc8a3aa99150c019508472fa3fa9ae19a11a
nsrcap <nsr.capital.007@gmail.com>
2026-06-11 02:19:00 +0200

    line plot

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
+"""Module that plots y as a line graph"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """plot y as a line graph"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    x = np.arange(0, 11)
-    plt.plot(x, y, 'r-')
+    plt.xlim(0, 10)
+    plt.plot(y, 'r-')
     plt.show()
+    plt.savefig('plot.png')
+
+
+line()
