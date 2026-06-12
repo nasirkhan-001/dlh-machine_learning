8794f2f5dceb243020d6976e7df599cf2aaefecf
nsrcap <nsr.capital.007@gmail.com>
2026-06-12 06:26:48 +0200

    creating histogram with controlled axis

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -11,8 +11,6 @@ def frequency():
     # generate 50 random data points with mean 68 and standard deviation 15
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-    # understood from visual provided in task, rather explicit statement
-    intervals = np.arange(0, 101, 10)
     # create x and y axis values auto using hist, with 50 random data points
     # each data point represent student grades
     # hist will auto calculate range of grades (min and max) from 50 random no.
@@ -20,7 +18,7 @@ def frequency():
     # from start 50+5=55 so first range is 50-55 and so on
     # this represent value on x-axis
     # Y-axis value, hist auto counts students falling in each range
-    plt.hist(student_grades, bins=intervals, edgecolor="black")
+    plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor="black")
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
