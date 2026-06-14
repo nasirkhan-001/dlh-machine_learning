ea62c3c0eca82c5008ce1f4308914da2d7aedf83
nsrcap <nsr.capital.007@gmail.com>
2026-06-14 20:52:37 +0200

    PDF for Exponential Distribution function

--- math/probability/3-main.py
+++ math/probability/3-main.py
@@ -5,8 +5,16 @@ Exponential = __import__('exponential').Exponential
 
 np.random.seed(0)
 data = np.random.exponential(0.5, 100).tolist()
+# task.3
 e1 = Exponential(data)
 print('Lambtha:', e1.lambtha)
 
 e2 = Exponential(lambtha=2)
 print('Lambtha:', e2.lambtha)
+
+# task.4
+e1 = Exponential(data)
+print('f(1):', e1.pdf(1))
+
+e2 = Exponential(lambtha=2)
+print('f(1):', e2.pdf(1))
--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -5,18 +5,23 @@
 class Exponential:
     """This class represents an exponential distribution"""
     # if data is given, the lambtha is calculated from data,
-    # else it will be provided by the user, by default it is 1.0
-    # lambtha is the rate of no of event over time
-    # while waiting time = 1/λ for exponential SF
+    # else it will be provided by the user, by default lambdha is 1.0
+    # lambtha is the rate of events occure for given time
+    # while λ is rate paraemter and is λ = 1/mean for exponential. DF
     # time matter in this distribution, its a continous random variable
-    # e.g. waiting time for call, bus, failure,increase
-    # so your waiting for the next event to happens
-    # that why decay (-lambdha in exponent) matters in probability
-    # becuase time will get decrease when we start waiting for event
-    # whats the probability that next emails will arive in 10mins
-    # lambdha = 5 emails (Event) / next hour (time)
-    # finance. e.g. time btw no of trades, time btw credit default
-    # how long next default will occure
+    # e.g. waiting time for call, bus, failure, accident
+    # focus is to find probabiltiy of waiting time untill next event
+    # that why decay used (-lambdha in exponent) in Exp.Dist.Func.
+    # becuase possibility of still wating will get decrease over time
+    # not time itself
+    # whats the probability that next emails will arive within 10mins
+    # probability that next emails will arive in 10mins = not possible
+    # becuase time is continous random variable and probability of
+    # time itself at specific point in time in future is not possible
+    # core concept difference is waiting time vs time
+    # finance. e.g. prob of time btw no of trades, of time btw credit default
+    # how long it will take that next trade/default will occure
+    # find probability of waiting time for given interval of time
     def __init__(self, data=None, lambtha=1.):
         """constructor method."""
         # attaching data to object "self or P1, P2 per checker file"
@@ -34,4 +39,15 @@ class Exponential:
                 Sum = 0
                 for i in range(len(data)):
                     Sum += data[i]
+            # lambdha = 1/mean for exponential DF
             self.lambtha = 1 / (Sum / len(data))
+
+    def pdf(self, x):
+        """find PDF where dansity=likelihood for give time period."""
+        if x < 0:
+            return 0
+        else:
+            # calculate PDF using the formula: (lambdha * e^-lambtha.Time)
+            e = 2.7182818285
+            pdf = ((self.lambtha) * e ** ((-self.lambtha) * x))
+            return pdf
