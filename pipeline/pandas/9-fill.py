bbe237b20fc5ed509044b93f5b46d9537226b3a5
nsrcap <nsr.capital.007@gmail.com>
2026-07-09 22:11:09 +0200

    filling missing values in col and row of pandas DF

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -4,10 +4,21 @@
 
 def fill(df):
     """Function that fill empty cells in row and column of DF."""
+    # remove weighted price col at all and 1 represent col index
     df = df.drop(['Weighted_Price'], axis=1)
+
+    # doing forward fill for close column
     df['Close'] = df['Close'].ffill()
-    df[['High', 'Low', 'Open']] = df[[
-        'High', 'Low', 'Open']].fillna(df['Close'])
+
+    # it was not possible to fill each column with close value in 
+    # one go like, see below 
+    # df[['High', 'Low', 'Open']] = df[['High', 'Low', 'Open']].fillna(df['Close'])
+    df['High'] = df['High'].fillna(df['Close'])
+    df['Low'] = df['Low'].fillna(df['Close'])
+    df['Open'] = df['Open'].fillna(df['Close'])
+
+    # its possible to fill multiple column with empty cell 
+    # with 0 in one go, see below 
     df[['Volume_(BTC)', 'Volume_(Currency)']] = df[[
         'Volume_(BTC)', 'Volume_(Currency)']].fillna(0)
     return df
