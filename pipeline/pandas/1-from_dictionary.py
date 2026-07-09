ad5fc23127d414cd2fd09484ab0af5d06fa4d00d
nsrcap <nsr.capital.007@gmail.com>
2026-07-09 10:27:43 +0200

    creating pandas dataframe from dictionary without defining function and print and return command

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -5,4 +5,4 @@ import pandas as pd
 df = pd.DataFrame({
     'First': [0.0, 0.5, 1.0, 1.5],
     'Second': ['one', 'two', 'three', 'four']
-    }, index=['a', 'b', 'c', 'd'])
+    }, index=['A', 'B', 'C', 'D'])
