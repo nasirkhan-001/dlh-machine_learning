f993d44f4be3183cc7a1e38fc8e0854d59161c0a
nsrcap <nsr.capital.007@gmail.com>
2026-07-09 18:16:04 +0200

    modifying orignal DF using rename function

---
+++ pipeline/pandas/3-rename.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module that use function to rename pandas DataFrame."""
+import pandas as pd
+
+
+def rename(df):
+    """Function that modify pandas DataFrame."""
+    # syntax for renaming function are around 10 and most common used are:
+    # index, columns, inplace, level, errors, copy, axis, mapper, & others.
+    # index to play with rows lables while columns is around headers
+    # inplace is to make changes in original DF, by default its false
+    # which means dont modify orignal DF and return new DF with changes
+    # level is to rename the index of a MultiIndex DataFrame
+    # errors is to handle raise them or ignore them upon changes in
+    # in DF, and by default it ignores errors
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit='s')
+    new_df = df.rename(columns={'Timestamp': 'Datetime'})
+    result_df = new_df(['Datetime', 'Close'])
+    return result_df
