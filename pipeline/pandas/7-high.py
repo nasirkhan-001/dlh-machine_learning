5f82e3f66ea7ddb3fb6645c0e2b504f40922f073
nsrcap <nsr.capital.007@gmail.com>
2026-07-09 19:41:11 +0200

    sorting value of particular column in DF

--- pipeline/pandas/7-high.py
+++ pipeline/pandas/7-high.py
@@ -4,8 +4,5 @@
 
 def high(df):
     """Function that sort values in particular column of DataFrame."""
-    # no panda import needed as df is defined already
-    # sort_index will work on row modification
-    # sort_value will work on values modification
     df = df['High'].sort_values(descending=True)
     return df
