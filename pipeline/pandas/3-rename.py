4521e2cc6972a787120cf7b3cf353a2818830fcc
nsrcap <nsr.capital.007@gmail.com>
2026-07-09 18:37:57 +0200

    modifying orignal DF using rename function[B

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -6,6 +6,6 @@ import pandas as pd
 def rename(df):
     """Function that modify pandas DataFrame."""
     df = df.rename(columns={'Timestamp': 'Datetime'})
-    df['Datetime'] = pd.to_datetime(df['Datatime'], unit='s')
+    df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
     df = df[['Datetime', 'Close']]
     return df
