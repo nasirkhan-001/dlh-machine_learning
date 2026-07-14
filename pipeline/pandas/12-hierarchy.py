efc099b9d6013e021f7bb0afac46dbceba2facc5
NK <nsr.capital.007@outlook.com>
2026-07-14 22:01:58 +0200

    concat two DF and swap multi-indexing

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -8,7 +8,16 @@ def hierarchy(df1, df2):
     """Task that apply hierarchical indexing over DataFrames."""
     df1 = index(df1)
     df2 = index(df2)
-    df2 = df2.loc[:1417411920]
+    df1 = df1.loc[1417411980:1417417980]
+    df2 = df2.loc[1417411980:1417417980]
     df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
-        
+    # now df is multiindex, like outer level or grouping is by
+    # bitstamp and coinbase while secondly index by timestamp
+    # column values and syntax for df.swaplevel(exchange, timestamp)
+    # df.swaplevel(col_name_with_index_to_change, new_index_col_name)
+    # 0 is outer index, 1 is inner, and if 2, it will more inner
+    # swap will simply move timestamp col as first index while
+    # bitstamp and coinbase column as secind index
+    df = df.swaplevel(0, 1)
+    df = df.sort_index()
     return df
--- pipeline/pandas/12-main.py
+++ pipeline/pandas/12-main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 from_file = __import__('2-from_file').from_file
-concat = __import__('11-concat').concat
+hierarchy = __import__('12-hierarchy').hierarchy
 
 df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
 
-df = concat(df1, df2)
+df = hierarchy(df1, df2)
 
 print(df)
