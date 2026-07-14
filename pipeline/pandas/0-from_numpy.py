0e4c55dae4e875baa6ab06e98e5307ad00d28e84
NK <nsr.capital.007@outlook.com>
2026-07-14 11:41:05 +0200

    from array to table

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python3
+"""this module is about convering numpy array to dataframe"""
+import pandas as pd
+
+
 def from_numpy(array):
-    """
-    Convert a NumPy array to a PyTorch tensor.
-
-    Args:
-        array (numpy.ndarray): The input NumPy array.
-
-    Returns:
-        torch.Tensor: The converted PyTorch tensor.
-    """
-    import torch
-    return torch.from_numpy(array)
+    """Convert a NumPy array to a Dataframe."""
+    no_of_col = array.shape[1]
+    if array.shape[1] > 26:
+        raise ValueError("array can not exceed 26 columns.")
+    else:
+        heading = []
+        for i in range(no_of_col):
+            heading.append(chr(i + 65))
+        return pd.DataFrame(array, columns=heading)
