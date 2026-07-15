d2d00349387ad4ea73a832cb91dd8de424eeaad4
NK <nsr.capital.007@outlook.com>
2026-07-15 03:00:37 +0200

    visualise whole data frame after receiving it from csv file

--- pipeline/pandas/14-visualize.py
+++ pipeline/pandas/14-visualize.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
+"""This module visualizes cryptocurrency data using pandas and matplotlib"""
 import matplotlib.pyplot as plt
 import pandas as pd
+
+
 from_file = __import__('2-from_file').from_file
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-df = df.drop(columns="Weighted_Price")
+df = df.drop(columns='Weighted_Price')
 df = df.rename(columns={"Timestamp": "Date"})
 df['Date'] = pd.to_datetime(df["Date"], unit="s")
-df = df.set_index("Date")
-df["Close"] = df["Close"].ffill()
-df["High"] = df["High"].fillna(["Close"])
-df["Low"] = df["Low"].fillna(["Close"])
-df["Open"] = df["Open"].fillna(["Close"])
-df[["Volume_(BTC)", "Volume_(Currency)"]] = df[[
-    "Volume_(BTC)", "Volume_(Currency)"]].fillna(0)
-df = df.loc[2017:]
+df = df.set_index('Date')
+df['Close'] = df['Close'].ffill()
+df['High'] = df['High'].fillna(df['Close'])
+df['Low'] = df['Low'].fillna(df['Close'])
+df['Open'] = df['Open'].fillna(df['Close'])
+df['Volume_(Currency)'] = df['Volume_(Currency)'].fillna(0)
+df['Volume_(BTC)'] = df['Volume_(BTC)'].fillna(0)
+df = df.loc["2017":]
 df = df.resample("D").agg({
-    "High": "max"
-    "Low": "min"
-    "Open": "mean"
-    "Close": "mean"
-    "Volume_(BTC): "sum"
-    "Volume_(Currency): "sum"
-    })
+    "High": "max",
+    "Low": "min",
+    "Open": "mean",
+    "Close": "mean",
+    "Volume_(BTC)": "sum",
+    "Volume_(Currency)": "sum"})
 df.plot()
-df.show()
+plt.savefig("14-Visualisation.png")
+plt.show()
